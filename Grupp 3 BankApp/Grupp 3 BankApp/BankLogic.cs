ca209571f1138954add071e7566f851e21554a28
Andras B <andreas.borang@student.kyh.se>
2021-10-05 10:45:47 +0200

    Started on Textfiles

--- Grupp 3 BankApp/Grupp 3 BankApp/BankLogic.cs
+++ Grupp 3 BankApp/Grupp 3 BankApp/BankLogic.cs
@@ -1,10 +1,37 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Text;
 
 namespace Grupp_3_BankApp
 {
     class BankLogic
     {
+
+
+
+
+
     }
+
+
+    private List<string> ReadCustomerFile()
+    {
+        /*
+         * File formating:
+         * Namn - ååååmmddxxxx ; konto , saldo : konto2 , saldo
+        */
+
+        List<string> CustomerList = new List<string>();
+
+        if (!File.Exists("Customers.txt"))
+        {
+            File.Create("Customers.txt");
+            return CustomerList;
+        }
+        return CustomerList;
+    }
+
+
+
 }
