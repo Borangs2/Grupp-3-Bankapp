0954642f195123b13490a336bd2da0d6a270cccd
Andras B <andreas.borang@student.kyh.se>
2021-10-05 14:12:05 +0200

    Added more functions in Banklogik

--- Grupp 3 BankApp/Grupp 3 BankApp/BankLogic.cs
+++ Grupp 3 BankApp/Grupp 3 BankApp/BankLogic.cs
@@ -8,9 +8,12 @@ namespace Grupp_3_BankApp
     class BankLogic
     {
 
+        private List<Customer> GlobalCustomerList = new List<Customer>();          
 
-
-
+        public BankLogic()
+        {
+            InterpretFile(ReadCustomerFile());
+        }
 
 
 
@@ -54,18 +57,60 @@ namespace Grupp_3_BankApp
 
         }
 
+        public bool ChangeCustomerName(string newName, int prsnNumber)
+        {
+
+            List<Customer> CustomerList = GlobalCustomerList;
+            
+            foreach(Customer customer in CustomerList)
+            {
+                /*
+                if (customer.Prsnnumber == prsnNumber)
+                {
+
+                    Console.WriteLine("bra");
+
+
+
+                }
+
+                //Ändra filen
+                */
+            }         
+            return false;
+        }
+
         //TODO: När customer klassen är klar lägg till detta
         private List<Customer> InterpretFile(List<string> CustomerFile)
         {
-            throw new NotImplementedException();
+            Console.WriteLine("Interpreting...");
+
             List<Customer> CustomerList = new List<Customer>();
+            string[] getName = new string[2];
+            string[] getPrsnNumber = new string[2];
+            List<string> getAccounts = new List<string>();
+
+            for(int i = 0; i < CustomerFile.Count; i++)
+            {
+                getName = CustomerFile[i].Split(" - ");
+                getPrsnNumber = getName[1].Split(" ; ");
+                string[] tempAccount = getPrsnNumber[1].Split(" : ");
+
+                foreach (string account in tempAccount)
+                {
+                    getAccounts.Add(account);
+                }
+            }
+
+
+
             foreach(string customer in CustomerFile)
             {
+                new Customer { 
                 
-
-
-
+                };
             }
+            return CustomerList;
         }
         private List<string> ReadCustomerFile()
         {
@@ -73,6 +118,8 @@ namespace Grupp_3_BankApp
              * File formating:
              * Namn - ååååmmddxxxx ; konto , saldo : konto2 , saldo
             */
+
+            Console.WriteLine("Reading...");
 
             List<string> CustomerList;
 
