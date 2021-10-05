c885b79b1fab330a1eda6fad725269b4173dbce3
Andras B <andreas.borang@student.kyh.se>
2021-10-05 11:09:00 +0200

    Gjorde färdigt ReadCustomerFile klassen och började på filhanteringen.

--- Grupp 3 BankApp/Grupp 3 BankApp/BankLogic.cs
+++ Grupp 3 BankApp/Grupp 3 BankApp/BankLogic.cs
@@ -12,26 +12,38 @@ namespace Grupp_3_BankApp
 
 
 
-    }
 
 
-    private List<string> ReadCustomerFile()
-    {
-        /*
-         * File formating:
-         * Namn - ååååmmddxxxx ; konto , saldo : konto2 , saldo
-        */
-
-        List<string> CustomerList = new List<string>();
-
-        if (!File.Exists("Customers.txt"))
+        //TODO: När customer klassen är klar lägg till detta
+        private List<Customer> InterpretFile(List<string> CustomerFile)
         {
-            File.Create("Customers.txt");
+            throw new NotImplementedException();
+            List<Customer> CustomerList = new List<Customer>();
+            foreach(string customer in CustomerFile)
+            {
+                
+
+
+
+            }
+        }
+        private List<string> ReadCustomerFile()
+        {
+            /*
+             * File formating:
+             * Namn - ååååmmddxxxx ; konto , saldo : konto2 , saldo
+            */
+
+            List<string> CustomerList;
+
+            if (!File.Exists("Customers.txt"))
+            {
+                File.Create("Customers.txt");
+                return CustomerList = new List<string>();
+            }
+            string[] ReadCustomerFile = File.ReadAllLines("Customers.txt");
+            CustomerList = new List<string>(ReadCustomerFile);
             return CustomerList;
         }
-        return CustomerList;
     }
-
-
-
 }
