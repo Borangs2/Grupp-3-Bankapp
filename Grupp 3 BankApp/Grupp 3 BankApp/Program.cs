1c73677b80bf5f804fc002cad3e8fdb64774198c
Hamza Essaid <hamza.essaid@student.kyh.se>
2021-10-07 11:39:47 +0200

    interface

--- Grupp 3 BankApp/Grupp 3 BankApp/Program.cs
+++ Grupp 3 BankApp/Grupp 3 BankApp/Program.cs
@@ -8,8 +8,97 @@ namespace Grupp_3_BankApp
         {
             BankLogic test = new BankLogic();
             test.Startup();
+            Program program = new Program();
+            program.Menu();
 
-
+        }
+        public void Menu()
+        {
+            
+            Console.WriteLine("Welcome to KYH BANK, please enter your Social Security number... (YYYY/MM/DD-NNNN)");
+            string prsnnmr = Console.ReadLine();
+            int PrsnNumber = (int)Convert.ToInt64(prsnnmr);
+            Console.WriteLine("Enter 1 to manage your account, 2 to edit your account, 3 to get quick info about your account or 4 to close KYH BANK.");
+            Console.WriteLine("(for swedish please press 9)");
+            string StringMenu1 = Console.ReadLine();
+            int Choice = Convert.ToInt32(StringMenu1);
+            switch (Choice)
+            {
+                case 1:
+                    Console.WriteLine("Enter 1 to create a new account, 2 to see info about your account, 3 to deposit, 4 to withdraw or 5 to close an account.");
+                    Console.WriteLine("(Enter 6 to get back to the main menu)");
+                    string StringMenu3 = Console.ReadLine();
+                    int NextChoice = Convert.ToInt32(StringMenu3);
+                    switch (NextChoice)
+                    {
+                        case 1:
+                            //AddNewAccount();
+                            break;
+                        case 2:
+                            //GetCustomer();
+                            break;
+                        case 3:
+                            //Insättning
+                            break;
+                        case 4:
+                            //Uttag
+                            break;
+                        case 5:
+                            //CloseAccount
+                            break;
+                        case 6:
+                            Menu();
+                            break;
+                    }
+                    break;
+                case 2:
+                    Console.WriteLine("Enter 1 to change name or 2 to remove customer. ");
+                    Console.WriteLine("(Enter 6 to get back to the main menu)");
+                    string StringMenu2 = Console.ReadLine();
+                    int NextChoice1 = Convert.ToInt32(StringMenu2);
+                    switch (NextChoice1) { 
+                        case 1:
+                            //ChangeName();
+                            break;
+                        case 2:
+                            //RemoveCustomer();
+                            break;
+                        case 3:
+                            Menu();
+                            break;
+                    }
+                    break;
+                        case 3:
+                    //GetCustomer();
+                    break;
+                case 4:
+                    //CloseApp();
+                    break;
+                case 6:
+                    //Admin meny
+                    Console.WriteLine("1 add acc, 2 all customers, 3 main menu, 4 close.");
+                    string AdminMenu = Console.ReadLine();
+                    int AdminChoice = Convert.ToInt32(AdminMenu);
+                    switch (AdminChoice)
+                    {
+                        case 1:
+                            //addaccount();
+                            break;
+                        case 2:
+                            //AllCustomers();
+                            break;
+                        case 3:
+                            Menu();
+                            break;
+                        case 4:
+                            //closeapp();
+                            break;
+                    }
+                    break;
+                case 9:
+                    //ChangeLang();
+                    break;
+            }
         }
     }
 }
